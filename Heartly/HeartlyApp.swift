e2c87fe0e1daa452e73caf332592b82d03f2d0b2
Lokhande Vivek <111556464+lvivek260@users.noreply.github.com>
2026-01-10 12:07:28 +0530

    first commit

---
+++ Heartly/HeartlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartlyApp.swift
+//  Heartly
+//
+//  Created by Vivek Lokhande on 10/01/26.
+//
+
+import SwiftUI
+
+@main
+struct HeartlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
